a1b5630bc24a2e36f2a1f8d2ee607f9e3746926a
mohammedabdulsami <mohammed.abdulsami@saal.ai>
2020-07-21 02:47:24 +0400

    Update seach-bar.js

--- src/js/components/seach-bar.js
+++ src/js/components/seach-bar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { TextField, Link } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 import { makeStyles } from '@material-ui/core/styles';
-
+import { createFilterOptions } from '@material-ui/lab/Autocomplete';
 import { search } from '../services/tmdb-service';
 
 const useStyles = makeStyles({
@@ -32,11 +32,14 @@ const SearchBar = () => {
     const [searchTxt, setSearchTxt] = useState('');
     const handleTextChange = (updatedText) => {
         setSearchTxt(updatedText);
-        search(updatedText)
-            .then((response) => {
-                setSearchedMovies(response.results);
-            })
-            .catch((err) => { console.error('search err', err) });
+        const trimmedText = updatedText.trim();
+        if (trimmedText.length > 0) {
+            search(trimmedText)
+                .then((response) => {
+                    setSearchedMovies(response.results);
+                })
+                .catch((err) => { console.error('search err', err) });
+        }
     };
 
 
@@ -48,15 +51,21 @@ const SearchBar = () => {
                 root: classes.root,
                 option: classes.option,
             }}
+            filterOptions={createFilterOptions({
+                trim: true,
+                stringify: option => `${option.original_title} ${option.title}`,
+            })}
             inputValue={searchTxt}
             autoHighlight
+            autoComplete
             freeSolo
+            disableListWrap
             disableClearable
             getOptionLabel={(option) => option.original_title}
             renderOption={(option) => (
                 <Link href={`/movie/${option.id}`}>
-                        {option.poster_path ? <span><img width={50} src={option.poster_full_path} /></span> : <span style={{ width: '50px' }} />}
-                        <span>{option.original_title} {option.release_date}</span>
+                    {option.poster_path ? <span><img width={50} src={option.poster_full_path} /></span> : <span style={{ width: '50px' }} />}
+                    <span>{option.original_title} {option.release_date}</span>
                 </Link>
             )}
             renderInput={(params) => (
@@ -64,6 +73,7 @@ const SearchBar = () => {
                     {...params}
                     label="Search"
                     variant="filled"
+                    type="name"
                     fullWidth
                     inputProps={{
                         ...params.inputProps,
